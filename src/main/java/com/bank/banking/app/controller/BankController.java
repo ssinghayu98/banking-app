8533b40a32ad7d4c89f1a9af3c19293f2d33e43f
Ayush Singh <ssinghayu98@gmail.com>
2026-04-10 21:03:02 +0530

    Updated BankController with validation and ApiResponse for balance

--- src/main/java/com/bank/banking/app/controller/BankController.java
+++ src/main/java/com/bank/banking/app/controller/BankController.java
@@ -1,10 +1,12 @@
 package com.bank.banking.app.controller;
 
 import com.bank.banking.app.dto.AmountRequest;
+import com.bank.banking.app.dto.ApiResponse;
 import com.bank.banking.app.dto.TransactionResponseDto;
 import com.bank.banking.app.dto.TransferRequest;
 import com.bank.banking.app.model.User;
 import com.bank.banking.app.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -19,25 +21,30 @@ public class BankController {
     private final UserService userService;
 
     @PostMapping("/deposit")
-    public String deposit(@RequestBody AmountRequest request, Authentication authentication) {
+    public String deposit(@Valid @RequestBody AmountRequest request, Authentication authentication) {
         User user = userService.getUserByUsername(authentication.getName());
         return userService.deposit(user.getId(), request.getAmount());
     }
 
     @PostMapping("/withdraw")
-    public String withdraw(@RequestBody AmountRequest request, Authentication authentication) {
+    public String withdraw(@Valid @RequestBody AmountRequest request, Authentication authentication) {
         User user = userService.getUserByUsername(authentication.getName());
         return userService.withdraw(user.getId(), request.getAmount());
     }
 
     @GetMapping("/balance")
-    public Double checkBalance(Authentication authentication) {
+    public ApiResponse<Double> checkBalance(Authentication authentication) {
         User user = userService.getUserByUsername(authentication.getName());
-        return userService.checkBalance(user.getId());
+        Double balance = userService.checkBalance(user.getId());
+
+        return new ApiResponse<>(
+                "Balance fetched successfully",
+                balance
+        );
     }
 
     @PostMapping("/transfer")
-    public String transfer(@RequestBody TransferRequest request, Authentication authentication) {
+    public String transfer(@Valid @RequestBody TransferRequest request, Authentication authentication) {
         User user = userService.getUserByUsername(authentication.getName());
 
         return userService.transferMoney(
